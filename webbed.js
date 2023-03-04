d900f27ed481e318ca5b55cd84b5b3f9f22522c9
Charankumar-E-G-D <122544631+Charankumar-E-G-D@users.noreply.github.com>
2023-03-04 15:41:05 +0530

    Add files via upload

---
+++ webbed.js
@@ -0,0 +1,44 @@
+function game()
+            {
+                var a=document.getElementById("if");
+                var arr=["if1.html","if2.html","if3.html"];
+                count=0;
+                document.getElementById("first_page").style.display="none";
+                a.style.display="block";
+                a.style.width="400px";
+                a.style.height="400px";
+                
+              
+                a.src="if1.html"
+               
+                
+            }
+function game1()
+            {
+                var a=document.getElementById("frame1");
+                var arr=["if1.html","if2.html","if3.html"];
+                count=0;
+                document.getElementById("first_page").style.display="none";
+                a.style.display="block";
+                a.style.width="400px";
+                a.style.height="400px";
+                
+              
+                a.src="if2.html"
+            
+            }
+function game2()
+            {
+                var a=document.getElementById("if2");
+                var arr=["if1.html","if2.html","if3.html"];
+                count=0;
+                document.getElementById("first_page").style.display="none";
+                a.style.display="block";
+                a.style.width="400px";
+                a.style.height="400px";
+                
+              
+                a.src="if3.html"
+               
+                
+            }
